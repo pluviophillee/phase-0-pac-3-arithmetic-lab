43c0d2553c142cc7c15059c88d72ff0ecd960171
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-04-05 17:55:38 +0300

    Completed assignment but I have confusion about the last two tests

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return (a + b);
+}
+
+function divide(a, b) {
+  return (a / b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function increment(n) {
+    return (++n);
+}
+
+function decrement(n) {
+    return (--n);
+}
+
+function makeInt(n=555){
+    parseInt(n, 10);
+}
+
+function preserveDecimal(){
+    parseFloat("80.123999"); 
+}
